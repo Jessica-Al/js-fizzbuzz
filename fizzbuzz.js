51d22e131906942f17078a20d52836764d5ebff9
Jessica-Al <jessca.allen@softwire.com>
2024-07-02 09:49:11 +0100

    code tidy

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -11,25 +11,30 @@ function fizzbuzz() {
     for (let count = 1; count <= 100; count ++) {
         //false if number replaced by fizz or buzz on output
         let no_multiples = true;
+
         //fizz check
         if (count % fizz == 0) {
-            no_multiples = false
+            no_multiples = false;
             process.stdout.write("fizz");
         }
+
         //buzz check
         if (count % buzz == 0) {
             no_multiples = false
             process.stdout.write("buzz");
         }
+
         //bang check
         if (count % bang == 0) {
             no_multiples = false;
             process.stdout.write("bang");
         }
+
         //not a multiple of `fizz` or `buzz`
         if (no_multiples) {
             process.stdout.write(count.toString());
         }
+        
         //newline for next number
         process.stdout.write("\n");
     }
