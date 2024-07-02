8ef0faa50facba60081e02396e0f4632f9a64418
Jessica-Al <jessca.allen@softwire.com>
2024-07-02 09:32:25 +0100

    Basic fizzbuzz (3 & 5)

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,30 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
+    console.log("WELCOME TO FIZZBUZZ");
 
-    // Put your code here...
+    //initialise multiples to check
+    const fizz = 3
+    const buzz = 5
+
+    //initilaise counter
+    for (let count = 1; count <= 100; count ++) {
+        //fizzbuzz check
+        if (count % fizz == 0 && count % buzz == 0) {
+            console.log("fizzbuzz");
+        }
+        //buzz check
+        else if (count % buzz == 0) {
+            console.log("buzz");
+        }
+        //fizz check
+        else if (count % fizz == 0) {
+            console.log("fizz");
+        }
+        //not a multiple of 3 and/or 5
+        else {
+            console.log(count);
+        }
+    }
 
 }
 
