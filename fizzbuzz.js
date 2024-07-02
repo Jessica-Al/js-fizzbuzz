903afcf4f25ab387c33a1175b598472d2a9be2a6
Jessica-Al <jessca.allen@softwire.com>
2024-07-02 10:03:20 +0100

    fizzbuzzbang

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,26 +6,37 @@ function fizzbuzz() {
     const fizz = 3;
     const buzz = 5;
     const bang = 7;
+    const bong = 11;
 
     //initilaise counter
     for (let count = 1; count <= 100; count ++) {
         //false if number replaced by fizz or buzz on output
         let no_multiples = true;
 
+        //set to true if bong is printed
+        let bong_number = false;
+
+        //bong check
+        if (count % bong == 0) {
+            no_multiples = false;
+            bong_number = true;
+            process.stdout.write("bong");
+        }
+
         //fizz check
-        if (count % fizz == 0) {
+        if (count % fizz == 0 && bong_number == false) {
             no_multiples = false;
             process.stdout.write("fizz");
         }
 
         //buzz check
-        if (count % buzz == 0) {
+        if (count % buzz == 0 && bong_number == false) {
             no_multiples = false
             process.stdout.write("buzz");
         }
 
         //bang check
-        if (count % bang == 0) {
+        if (count % bang == 0 && bong_number == false) {
             no_multiples = false;
             process.stdout.write("bang");
         }
@@ -34,7 +45,7 @@ function fizzbuzz() {
         if (no_multiples) {
             process.stdout.write(count.toString());
         }
-        
+
         //newline for next number
         process.stdout.write("\n");
     }
